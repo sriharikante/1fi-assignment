b4a87b9c66b6371792c85eef7b9cf4ccc8df2e2d
sriharikante <kantesrihari1@gmail.com>
2026-09-03 01:21:12 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -47,7 +47,7 @@ app.use("/api/products", productRoutes);
 app.use(express.static(frontendDistPath));
 
 // React Router fallback
-app.get("*", (req, res) => {
+app.get("/{*splat}", (req, res) => {
   res.sendFile(path.join(frontendDistPath, "index.html"));
 });
 
